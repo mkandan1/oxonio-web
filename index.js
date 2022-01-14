96f34e478e6b82d0cd73acb1e1bb7eebd3efcf88
Mani Kandan <64264387+mkandan1@users.noreply.github.com>
2022-01-14 20:58:49 +0530

    Initial Commit ✨

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+var url = require('url');
 const app = express();
 const port = 3000;
 
@@ -7,12 +8,21 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use('/static', express.static('public'));
+
 app.get('/', (req, res) => {
     res.render('index', {
         title: 'Oxonio | Shaping The Future',
         exampleMessage: 'Site is under construction'
     });
 });
+
+
+// Set 404 Error page
+app.use((req, res, next) => {
+    res.status(404).render('404');
+    res.end();
+})
 
 app.listen(process.env.PORT || port, () => {
     console.log(`Server booming at http://localhost:${port}`);
